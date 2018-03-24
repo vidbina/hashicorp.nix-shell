7f9e9ba9ea8afafc63d2fa6253f5abbadabdf77f
David Asabina <vid@bina.me>
2018-03-24 11:20:03 +0100

    fix: src-ing from git instead of local fs

--- default.nix
+++ default.nix
@@ -3,14 +3,16 @@
 stdenv.mkDerivation rec {
   name = "terraform-dev-${version}";
   src = ./.;
-  version = "0.1.0";
+  version = "0.11.5";
 
   buildInputs = with pkgs; [
     (terraform.overrideAttrs (old: rec {
       name = "terraform-${version}";
-      src = ../terraform;
-      version = "0.11.2-dev";
-      #sha256 = "testing";
+      src = fetchgit {
+        url = "https://github.com/hashicorp/terraform.git";
+        rev = "v${version}";
+        sha256 = "130ibb1pd60r2cycwpzs8qfwrz6knyc1a1849csxpipg5rs5q3jy";
+      };
     }))
   ];
 
