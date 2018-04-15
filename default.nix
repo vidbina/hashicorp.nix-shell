f15d5ff39ff948ffcf12d7055f96f364c1344c82
David Asabina <vid@bina.me>
2018-04-15 18:11:30 +0200

    added packer
    With Packer added this is no longer a Terraform env nix-shell
    description but more of a Hashicorp env nix-shell expression :wink:.

--- default.nix
+++ default.nix
@@ -1,18 +1,28 @@
 { stdenv, pkgs }:
 
 stdenv.mkDerivation rec {
-  name = "terraform-dev-${version}";
+  name = "hashicorp-env-${version}";
   src = ./.;
-  version = "0.11.5";
+  version = "0.1.0";
+  packer_version = "1.2.2";
+  terraform_version = "0.11.5";
 
   buildInputs = with pkgs; [
     go_1_9
     graphviz
+    (packer.overrideAttrs (old: rec {
+      name = "packer-${packer_version}";
+      src = fetchgit {
+        url = "https://github.com/hashicorp/packer.git";
+        rev = "v${packer_version}";
+        sha256 = "05k3hzp0456f7ygz0k0f2sz6xv32rdb9bg5fkr5s69af84p53d91";
+      };
+    }))
     (terraform.overrideAttrs (old: rec {
-      name = "terraform-${version}";
+      name = "terraform-${terraform_version}";
       src = fetchgit {
         url = "https://github.com/hashicorp/terraform.git";
-        rev = "v${version}";
+        rev = "v${terraform_version}";
         sha256 = "130ibb1pd60r2cycwpzs8qfwrz6knyc1a1849csxpipg5rs5q3jy";
       };
     }))
